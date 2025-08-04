9e35e9de2c9144fbf3eab8f5285a9586500545af
Hernán Rago <hernanrago@msn.com>
2025-08-04 16:28:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -87,5 +87,5 @@ app.get("/api/calculate-mep", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(\`Server running on http://localhost:\${PORT}\`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
